a50ed834d4f253e9d01017a57ebcd6c3033e4e22
Djailla <bastien.vallet@gmail.com>
2016-09-13 16:35:43 +0200

    Add screenshot Remove ARM support Add README.md

---
+++ README.md
@@ -0,0 +1,9 @@
+subsonic - NAS OS App
+==================================
+
+**subsonic** is an app to install on NASes running NAS OS (for Seagate or LaCie NAS).
+It's based on the [subsonic](http://www.subsonic.org/) project.
+
+subsonic is a web based service to share your media and store the files on the NAS.
+
+![Alt text](com.djailla.subsonic/resources/screenshots/en/screenshot-1.png?raw=true "Screenshot")
--- org.subsonic.subsonic/build.sh
+++ org.subsonic.subsonic/build.sh
@@ -2,11 +2,16 @@
 
 install -m 755 /home/source/rc.local /etc
 
-apt-get update
-apt-get install openjdk-7-jre -y
-wget http://subsonic.org/download/subsonic-6.0.deb -P /tmp
-dpkg -i /tmp/subsonic-6.0.deb
-rm /tmp/subsonic-6.0.deb
-update-rc.d -f subsonic remove
+if [ $RAINBOW_ARCHITECTURE = "x86_64" ]
+then
+    apt-get update
+    apt-get install openjdk-7-jre -y
+    wget http://subsonic.org/download/subsonic-6.0.deb -P /tmp
+    dpkg -i /tmp/subsonic-6.0.deb
+    rm /tmp/subsonic-6.0.deb
+    update-rc.d -f subsonic remove
+else
+    echo "No ARM support"
+fi
 
 exit 0
